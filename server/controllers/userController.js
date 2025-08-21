d26eedbdd17dafc641828ba7b922c49aade89627
Sandipan Malick <malicksandipan44@gmail.com>
2025-08-21 20:53:07 +0530

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -154,7 +154,7 @@ exports.login = async (req, res) => {
       .cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        sameSite: "none",
         maxAge: 24 * 60 * 60 * 1000, // 1 day
       })
       .status(200)
