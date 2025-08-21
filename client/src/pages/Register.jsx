0ed9f9e948143387d71870ce293ebe672484fd9e
Sandipan Malick <malicksandipan44@gmail.com>
2025-08-21 12:07:15 +0530

    Update Register.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 function Register() {
